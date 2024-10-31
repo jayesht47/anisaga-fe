d865f8b266530c7475a51dc56c32c69fedb2a104
Jayesh <jayeshthakur47@gmail.com>
2024-10-31 15:44:19 +0530

    added changes for login form

--- src/app/libs/actions.ts
+++ src/app/libs/actions.ts
@@ -1,6 +1,6 @@
 "use server";
 import { FormSchema, FormState } from "./definitions";
-import { registerUser } from "./user-apis";
+import { registerUser, loginUser } from "./user-apis";
 
 export async function signup(state: FormState, formData: FormData) {
   const validatedFields = FormSchema.safeParse({
@@ -15,4 +15,19 @@ export async function signup(state: FormState, formData: FormData) {
   }
   console.info("validaion sucess calling API");
   registerUser(validatedFields.data);
+}
+
+export async function login(state: FormState, formData: FormData) {
+  const validatedFields = FormSchema.safeParse({
+    userName: formData.get("userName"),
+    password: formData.get("password"),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+    };
+  }
+  console.info("validaion sucess calling API");
+  loginUser(validatedFields.data);
 }
--- src/app/libs/user-apis.ts
+++ src/app/libs/user-apis.ts
@@ -30,3 +30,36 @@ export const registerUser = async (user: User) => {
     console.info(`response json was ${JSON.stringify(respObj)}`);
   }
 };
+
+
+export const loginUser = async (user: User) => {
+  const hostname: string | undefined = process.env.SERVER_URL;
+  console.log(`process.env.SERVER_URL is ${process.env.SERVER_URL}`)
+  if (hostname == undefined) throw new Error("server url not configured!");
+  const registerUrl = hostname ? hostname + "/auth/login" : "";
+  console.info(`registerUrl is ${registerUrl}`)
+  const headers: Headers = new Headers();
+  headers.set("Content-Type", "application/json");
+  const response = await fetch(registerUrl, {
+    method: "POST",
+    body: JSON.stringify(user),
+    headers: headers,
+  });
+
+  if (response.status != 200) {
+    const respObj = await response.json();
+    console.error(
+      `receieved status ${
+        response.status
+      } for register user for ${JSON.stringify(
+        user
+      )} response json was ${JSON.stringify(respObj)}`
+    );
+  }
+
+  if (response.status === 200) {
+    const respObj = await response.json();
+    console.debug("registration successfull");
+    console.info(`response json was ${JSON.stringify(respObj)}`);
+  }
+};
