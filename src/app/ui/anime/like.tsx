390f53e0b0c22c03ffe3d8cdb43131d987cc1cd7
Jayesh <jayeshthakur47@gmail.com>
2025-03-04 19:04:02 +0900

    updated like component to be visible when logged in only

--- src/app/ui/anime/like.tsx
+++ src/app/ui/anime/like.tsx
@@ -1,43 +1,50 @@
 "use client";
+import { isLoggedIn } from "@/app/lib/actions";
 import {
-  addToLikes,
-  checkIfLiked,
-  removeFromLikes,
+    addToLikes,
+    checkIfLiked,
+    removeFromLikes,
 } from "@/app/lib/anime-apis";
+import { AuthContext } from "@/app/lib/auth-provider";
 import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { IoHeartOutline, IoHeartSharp } from "react-icons/io5";
 
 export default function Like() {
-  const [isLiked, setIsLiked] = useState(false);
-  const pathname: string = usePathname();
-  const slug: string = pathname.split("/")[2];
+    const [isLiked, setIsLiked] = useState(false);
+    const { authState, setAuthState } = useContext(AuthContext);
+    const pathname: string = usePathname();
+    const slug: string = pathname.split("/")[2];
 
-  useEffect(() => {
-    const getIsLiked = async () => {
-      setIsLiked(await checkIfLiked(slug));
+    useEffect(() => {
+        const getIsLiked = async () => {
+            setIsLiked(await checkIfLiked(slug));
+        };
+        const updateAuthState = async () => {
+            setAuthState(await isLoggedIn());
+        };
+        getIsLiked();
+        updateAuthState();
+    });
+    const likeClickHandler = async () => {
+        if (!isLiked) setIsLiked(await addToLikes(slug));
+        else setIsLiked(!(await removeFromLikes(slug)));
     };
-    getIsLiked();
-  });
-  const likeClickHandler = async () => {
-    if (!isLiked) setIsLiked(await addToLikes(slug));
-    else setIsLiked(!(await removeFromLikes(slug)));
-  };
 
-  return (
-    <>
-      {!isLiked && (
-        <IoHeartOutline
-          className="ml-5 hover:cursor-pointer"
-          onClick={likeClickHandler}
-        />
-      )}
-      {isLiked && (
-        <IoHeartSharp
-          className="ml-5 hover:cursor-pointer"
-          onClick={likeClickHandler}
-        />
-      )}
-    </>
-  );
+    return (
+        <>
+            {authState && !isLiked && (
+                <IoHeartOutline
+                    className="ml-5 hover:cursor-pointer"
+                    onClick={likeClickHandler}
+                />
+            )}
+            {authState && isLiked && (
+                <IoHeartSharp
+                    className="ml-5 hover:cursor-pointer"
+                    onClick={likeClickHandler}
+                />
+            )}
+        </>
+    );
 }
