00f73afa6ed1084e2b24c3143164d02b54f6c0a2
Jayesh <jayeshthakur47@gmail.com>
2025-03-04 17:35:45 +0900

    updated user dashboard

--- src/app/ui/dashboard/dashboard.tsx
+++ src/app/ui/dashboard/dashboard.tsx
@@ -1,23 +1,37 @@
 "use client";
 import { isLoggedIn } from "@/app/lib/actions";
+import { getLikedAnimes } from "@/app/lib/anime-apis";
 import { AuthContext } from "@/app/lib/auth-provider";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
+import Card from "../card";
+import { Anime } from "@/app/lib/definitions";
 
 export default function Dashboard() {
   const { authState, setAuthState } = useContext(AuthContext);
+  const [likedAnimes, setLikedAnimes] = useState<[Anime] | undefined>(
+    undefined
+  );
 
   useEffect(() => {
-    console.log(`useEffect called`);
-    isLoggedIn().then((newAuthState) => {
-      console.log(`newAuthState is ${newAuthState}`);
-      setAuthState(newAuthState);
-    });
-  });
+    const updateAuthState = async () => {
+      setAuthState(await isLoggedIn());
+    };
+    const setAnimeLikes = async () => {
+      setLikedAnimes(await getLikedAnimes());
+    };
+    updateAuthState();
+    if (authState) setAnimeLikes();
+  }, [authState, setAuthState]);
 
   return (
-    <div>
-      This is Dashbaord component
-      <div> {`The user logged in status is ${authState}`}</div>
+    <div className="m-10">
+      <div className="text-2xl">My Likes</div>
+      <div className="flex flex-wrap justify-center">
+        {authState &&
+          likedAnimes?.map((item, index) => (
+            <Card key={`card-${index.toString()}`} anime={item} />
+          ))}
+      </div>
     </div>
   );
 }
