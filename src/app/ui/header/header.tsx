6e32d2814d0ce7f9c895df1dae2a2f132d077e73
Jayesh <jayeshthakur47@gmail.com>
2025-03-06 19:16:52 +0900

    added search icon in header

--- src/app/ui/header/header.tsx
+++ src/app/ui/header/header.tsx
@@ -4,87 +4,96 @@ import { useRouter } from "next/navigation";
 import Button from "./button";
 import { isLoggedIn, logout } from "@/app/lib/actions";
 import { useEffect, useState } from "react";
+import { IoSearchOutline } from "react-icons/io5";
 
 export default function Header() {
-  const router = useRouter();
-  const [authState, setAuthState] = useState(false);
+    const router = useRouter();
+    const [authState, setAuthState] = useState(false);
 
-  useEffect(() => {
-    const updateAuthState = async () => {
-      setAuthState(await isLoggedIn());
+    useEffect(() => {
+        const updateAuthState = async () => {
+            setAuthState(await isLoggedIn());
+        };
+        updateAuthState();
+    });
+
+    const loginClickHandler = () => {
+        router.push("/login");
     };
-    updateAuthState();
-  });
 
-  const loginClickHandler = () => {
-    router.push("/login");
-  };
+    const DashboardClickHandler = () => {
+        router.push("/dashboard");
+    };
 
-  const DashboardClickHandler = () => {
-    router.push("/dashboard");
-  };
+    const signupClickHandler = () => {
+        router.push("/signup");
+    };
 
-  const signupClickHandler = () => {
-    router.push("/signup");
-  };
+    const logoutClickHandler = () => {
+        logout();
+    };
 
-  const logoutClickHandler = () => {
-    logout();
-  };
+    const logoClickHandler = () => {
+        router.push("/");
+    };
 
-  const logoClickHandler = () => {
-    router.refresh();
-    router.push("/");
-  };
+    const searchClickHandler = () => {
+        router.push("/search");
+    };
 
-  return (
-    <header>
-      <div className="flex justify-between">
-        <div
-          className="justify-self-start m-6 text-5xl select-none hover:cursor-pointer"
-          onClick={logoClickHandler}
-        >
-          AniSaga
-        </div>
-        <div className="justify-self-end">
-          {!authState && (
-            <Button
-              className="m-6 text-2xl"
-              type="button"
-              onClick={signupClickHandler}
-            >
-              Sign up
-            </Button>
-          )}
-          {!authState && (
-            <Button
-              className="m-6 text-2xl"
-              type="button"
-              onClick={loginClickHandler}
-            >
-              Login
-            </Button>
-          )}
-          {authState && (
-            <Button
-              className="m-6 text-2xl"
-              type="button"
-              onClick={DashboardClickHandler}
-            >
-              Dashboard
-            </Button>
-          )}
-          {authState && (
-            <Button
-              className="m-6 text-2xl"
-              type="button"
-              onClick={logoutClickHandler}
-            >
-              Logout
-            </Button>
-          )}
-        </div>
-      </div>
-    </header>
-  );
+    return (
+        <header>
+            <div className="flex justify-between">
+                <div
+                    className="justify-self-start m-6 text-5xl select-none hover:cursor-pointer"
+                    onClick={logoClickHandler}
+                >
+                    AniSaga
+                </div>
+                <div className="justify-self-end flex ">
+                    <IoSearchOutline
+                        size={35}
+                        className="m-6 self-center hover:cursor-pointer"
+                        onClick={searchClickHandler}
+                    />
+                    {!authState && (
+                        <Button
+                            className="m-6 text-2xl"
+                            type="button"
+                            onClick={signupClickHandler}
+                        >
+                            Sign up
+                        </Button>
+                    )}
+                    {!authState && (
+                        <Button
+                            className="m-6 text-2xl"
+                            type="button"
+                            onClick={loginClickHandler}
+                        >
+                            Login
+                        </Button>
+                    )}
+                    {authState && (
+                        <Button
+                            className="m-6 text-2xl"
+                            type="button"
+                            onClick={DashboardClickHandler}
+                        >
+                            Dashboard
+                        </Button>
+                    )}
+                    {authState && (
+                        <Button
+                            className="m-6 text-2xl"
+                            type="button"
+                            onClick={logoutClickHandler}
+                        >
+                            Logout
+                        </Button>
+                    )}
+                </div>
+            </div>
+        </header>
+    );
 }
